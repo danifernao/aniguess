27dcf4677937d1d813739124fdc23b1cfe55d116
danifernao <82724282+danifernao@users.noreply.github.com>
2026-05-13 18:35:56 -0500

    feat: convierte la app en PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      injectRegister: 'auto',
+      includeAssets: ['favicon.png', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'AniGuess',
+        short_name: 'AniGuess',
+        description: "Who's that character? Test your anime and manga knowledge in AniGuess.",
+        theme_color: '#0b1622',
+        background_color: '#0b1622',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ]
+      }
+    }),
+  ],
   base: "",
 })
